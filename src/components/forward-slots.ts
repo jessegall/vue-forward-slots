e376a3655c465e8ffe4757b9d4cff28b0275021a
Jesse Gall <jesse-gall@hotmail.com>
2024-08-04 22:04:18 +0200

    Set current instance to null when instance is null, code cleanup

--- src/components/forward-slots.ts
+++ src/components/forward-slots.ts
@@ -1,4 +1,12 @@
-import { computed, defineComponent, getCurrentInstance, h, PropType, useSlots, VNode } from 'vue';
+import {
+    ComponentInternalInstance,
+    computed,
+    defineComponent,
+    getCurrentInstance,
+    h,
+    PropType,
+    VNode
+} from 'vue';
 
 type SlotOption = string | string[];
 
@@ -7,26 +15,34 @@ interface ForwardSlotsProps {
     exclude?: SlotOption;
 }
 
-const isValidSlotOption = (value: any): value is SlotOption =>
-    typeof value === 'string' || Array.isArray(value);
+function isValidSlotOption(value: any): value is SlotOption {
+    return typeof value === 'string' || Array.isArray(value);
+}
 
-const normalizeToArray = (input: SlotOption | undefined): string[] =>
-    Array.isArray(input) ? input : input ? [input] : [];
+function normalizeToArray(input: SlotOption | undefined): string[] {
+    return Array.isArray(input) ? input : input ? [input] : [];
+}
 
-const createSlots = (slots: Record<string, Function>, options: ForwardSlotsProps) => {
+function createSlots(slots: Record<string, Function>, options: ForwardSlotsProps) {
     const include = normalizeToArray(options.slot);
     const exclude = normalizeToArray(options.exclude);
 
-    return Object.entries(slots)
-        .filter(([key]) =>
-            (! include.length || include.includes(key)) &&
-            (! exclude.length || ! exclude.includes(key))
-        )
-        .reduce((acc, [key, slot]) => ({ ...acc, [key]: (args: any) => slot(args) }), {});
-};
+    return Object.fromEntries(
+        Object.entries(slots)
+            .filter(([key]) => shouldIncludeSlot(key, include, exclude))
+            .map(([key, slot]) => [key, (args: any) => slot(args)])
+    );
+}
 
-const withForwardedSlots = (component: VNode | undefined, options: ForwardSlotsProps, slots: Record<string, Function>): VNode =>
-    h(component, options, createSlots(slots, options));
+function shouldIncludeSlot(key: string, include: string[], exclude: string[]): boolean {
+    if (include.length && ! include.includes(key)) return false;
+    if (exclude.length && exclude.includes(key)) return false;
+    return true;
+}
+
+function withForwardedSlots(component: VNode | undefined, options: ForwardSlotsProps, slots: Record<string, Function>): VNode {
+    return h(component, options, createSlots(slots, options));
+}
 
 const ForwardSlotsComponent = defineComponent({
     name: "ForwardSlots",
@@ -45,37 +61,39 @@ const ForwardSlotsComponent = defineComponent({
     setup(props: ForwardSlotsProps, ctx) {
         const children = computed(() => ctx.slots.default?.() || []);
 
-        return () => children.value.map(vNode => {
-            return withForwardedSlots(vNode, props, { ...(vNode as any).children, ...(ctx as any).__slots } || {})
-        });
+        return () => children.value.map(vNode => withForwardedSlots(vNode, props, {
+            ...(vNode as any).children,
+            ...(currentInstance?.slots || {})
+        }));
     }
 })
 
-let lastInstanceId: number = null;
-let currentSlots = {} as Record<string, Function>;
-
-const originalSetup = ForwardSlotsComponent.setup;
-ForwardSlotsComponent.setup = (props, ctx) => originalSetup(props, { ...ctx, __slots: currentSlots } as any)
+let currentInstance: ComponentInternalInstance = null;
 
 export const ForwardSlots = new Proxy(ForwardSlotsComponent, {
     get(target: any, prop: string | symbol) {
 
         // The __v_isRef property is accessed when Vue initializes the component.
-        // We use this as a hook to capture the correct slots for forwarding.
-        // At this point, useSlots() still references the context where ForwardSlots is used,
-        // allowing us to capture the intended parent slots before they're overwritten.
+        // We use this as a hook to capture the correct instance to forward slots from.
+        // At this point, getCurrentInstance() still references the instance where ForwardSlots is used,
+        // allowing us to capture the intended parent before it's overwritten.
         if (prop === '__v_isRef') {
-            const instance = getCurrentInstance();
-
-            if (instance && instance.uid !== lastInstanceId) {
-                currentSlots = useSlots();
-                lastInstanceId = instance.uid;
-            }
+            updateCurrentInstance(getCurrentInstance());
         }
 
         return target[prop];
     }
 });
 
+function updateCurrentInstance(instance: ComponentInternalInstance) {
+    if (! instance) {
+        currentInstance = null;
+        return;
+    }
+
+    if (! currentInstance || currentInstance.uid !== instance.uid) {
+        currentInstance = instance;
+    }
+}
 
 export default ForwardSlots;
